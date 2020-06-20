9ffd20980c5d9bd17cfe2f0d1f3fd5e1527823b6
Arash Zandi <arash.zandi@gmail.com>
2020-06-20 14:47:41 +0200

    Use the test and validation splits form the GUM data source
    Also DRY-up the code

--- scripts/make_gum.py
+++ scripts/make_gum.py
@@ -3,12 +3,10 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from parseridge.corpus.treebank import Treebank
-
-treebank = Treebank(
-    train_io=open("../UD_English-GUM/en_gum-ud-train.conllu"),
-    dev_io=open("../UD_English-GUM/en_gum-ud-dev.conllu"),
-    test_io=open("../UD_English-GUM/en_gum-ud-test.conllu")  # optional,
-)
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation
+from keras.optimizers import Adam
 
 def tag_sentences(corpus):
     for sentence in corpus.sentences:
@@ -16,103 +14,78 @@ def tag_sentences(corpus):
         for token in sentence.tokens:
             result.append((token.form, token.upostag))
         yield result
+ 
+def split(tagged_sentences):
+    sentences, sentence_tags =[], [] 
+    for tagged_sentence in tagged_sentences:
+        sentence, tags = zip(*tagged_sentence)
+        sentences.append(np.array(sentence))
+        sentence_tags.append(np.array(tags))
+    return sentences, sentence_tags
 
-tagged_sentences = tag_sentences(treebank.train_corpus)
- 
-sentences, sentence_tags =[], [] 
-for tagged_sentence in tagged_sentences:
-    sentence, tags = zip(*tagged_sentence)
-    sentences.append(np.array(sentence))
-    sentence_tags.append(np.array(tags))
+def load_data():
+    return Treebank(
+        train_io=open("../UD_English-GUM/en_gum-ud-train.conllu"),
+        dev_io=open("../UD_English-GUM/en_gum-ud-dev.conllu"),
+        test_io=open("../UD_English-GUM/en_gum-ud-test.conllu"))
 
- 
-(train_sentences, 
- test_sentences, 
- train_tags, 
- test_tags) = train_test_split(sentences, sentence_tags, test_size=0.2)
+def get_words(train_sentences):
+    words = set()
+    for s in train_sentences:
+        for w in s:
+            words.add(w.lower())
+    return words
 
-words, tags = set([]), set([])
- 
-for s in train_sentences:
-    for w in s:
-        words.add(w.lower())
- 
-for ts in train_tags:
-    for t in ts:
-        tags.add(t)
- 
-word2index = {w: i + 2 for i, w in enumerate(list(words))}
-word2index['-PAD-'] = 0  # The special value used for padding
-word2index['-OOV-'] = 1  # The special value used for OOVs
+def get_tags(train_tags):
+    tags = set()
+    for ts in train_tags:
+        for t in ts:
+            tags.add(t)
+    return tags
 
-tag2index = {t: i + 1 for i, t in enumerate(list(tags))}
-tag2index['-PAD-'] = 0  # The special value used to padding
+def get_word2index(words):
+    word2index = {w: i + 2 for i, w in enumerate(list(words))}
+    word2index['-PAD-'] = 0  # The special value used for padding
+    word2index['-OOV-'] = 1  # The special value used for OOVs
+    return word2index
 
-train_sentences_X, test_sentences_X, train_tags_y, test_tags_y = [], [], [], []
- 
-for s in train_sentences:
-    s_int = []
-    for w in s:
-        try:
-            s_int.append(word2index[w.lower()])
-        except KeyError:
-            s_int.append(word2index['-OOV-'])
- 
-    train_sentences_X.append(s_int)
- 
-for s in test_sentences:
-    s_int = []
-    for w in s:
-        try:
-            s_int.append(word2index[w.lower()])
-        except KeyError:
-            s_int.append(word2index['-OOV-'])
- 
-    test_sentences_X.append(s_int)
- 
-for s in train_tags:
-    train_tags_y.append([tag2index[t] for t in s])
- 
-for s in test_tags:
-    test_tags_y.append([tag2index[t] for t in s])
- 
-print(train_sentences_X[0])
-print(test_sentences_X[0])
-print(train_tags_y[0])
-print(test_tags_y[0])
+def get_tag2index(tags):
+    tag2index = {t: i + 1 for i, t in enumerate(list(tags))}
+    tag2index['-PAD-'] = 0  # The special value used to padding
+    return tag2index
 
-MAX_LENGTH = len(max(train_sentences_X, key=len))
-print(MAX_LENGTH)  # 271
+def get_x(sentences, word2index):
+    x = []
+    for s in sentences:
+        s_int = []
+        for w in s:
+            try:
+                s_int.append(word2index[w.lower()])
+            except KeyError:
+                s_int.append(word2index['-OOV-'])
+    
+        x.append(s_int)
+    return x
 
-from keras.preprocessing.sequence import pad_sequences
- 
-train_sentences_X = pad_sequences(train_sentences_X, maxlen=MAX_LENGTH, padding='post')
-test_sentences_X = pad_sequences(test_sentences_X, maxlen=MAX_LENGTH, padding='post')
-train_tags_y = pad_sequences(train_tags_y, maxlen=MAX_LENGTH, padding='post')
-test_tags_y = pad_sequences(test_tags_y, maxlen=MAX_LENGTH, padding='post')
- 
-print(train_sentences_X[0])
-print(test_sentences_X[0])
-print(train_tags_y[0])
-print(test_tags_y[0])
+def get_y(tags, tag2index):
+    y = []
+    for s in tags:
+        y.append([tag2index[t] for t in s])
+    return y
 
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation
-from keras.optimizers import Adam
-
-
-model = Sequential()
-model.add(InputLayer(input_shape=(MAX_LENGTH, )))
-model.add(Embedding(len(word2index), 128))
-model.add(Bidirectional(LSTM(256, return_sequences=True)))
-model.add(TimeDistributed(Dense(len(tag2index))))
-model.add(Activation('softmax'))
- 
-model.compile(loss='categorical_crossentropy',
-              optimizer=Adam(learning_rate=0.001),
-              metrics=['accuracy'])
- 
-model.summary()
+def get_model(word2index, tag2index):
+    model = Sequential()
+    model.add(InputLayer(input_shape=(MAX_LENGTH, )))
+    model.add(Embedding(len(word2index), 128))
+    model.add(Bidirectional(LSTM(256, return_sequences=True)))
+    model.add(TimeDistributed(Dense(len(tag2index))))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy',
+                optimizer=Adam(learning_rate=0.001),
+                metrics=['accuracy'])
+    
+    model.summary()
+    return model
 
 def to_categorical(sequences, categories):
     cat_sequences = []
@@ -124,14 +97,50 @@ def to_categorical(sequences, categories):
         cat_sequences.append(cats)
     return np.array(cat_sequences)
 
-cat_train_tags_y = to_categorical(train_tags_y, len(tag2index))
-print(cat_train_tags_y[0])
 
-model.fit(train_sentences_X, to_categorical(train_tags_y, len(tag2index)), batch_size=128, epochs=40, validation_split=0.2)
-scores = model.evaluate(test_sentences_X, to_categorical(test_tags_y, len(tag2index)))
+treebank = load_data()
+train_sentences, train_tags = split(tag_sentences(treebank.train_corpus))
+validation_sentences, validation_tags = split(tag_sentences(treebank.dev_corpus))
+test_sentences, test_tags = split(tag_sentences(treebank.test_corpus))
+
+words = get_words(train_sentences)
+tags = get_tags(train_tags)
+word2index = get_word2index(words)
+tag2index = get_tag2index(tags)
+
+train_sentences_X = get_x(train_sentences, word2index)
+test_sentences_X = get_x(test_sentences, word2index)
+validation_sentences_X = get_x(validation_sentences, word2index)
+train_tags_y = get_y(train_tags, tag2index)
+test_tags_y = get_y(test_tags, tag2index)
+validation_tags_y = get_y(validation_tags, tag2index)
+
+MAX_LENGTH = len(max(train_sentences_X, key=len))
+print(MAX_LENGTH)  # 271
+
+def pad(data, length):
+    return pad_sequences(data, maxlen=length, padding='post')
+
+train_sentences_X = pad(train_sentences_X, MAX_LENGTH)
+test_sentences_X = pad(test_sentences_X, MAX_LENGTH)
+validation_sentences_X = pad(validation_sentences_X, MAX_LENGTH)
+train_tags_y = pad(train_tags_y, MAX_LENGTH)
+test_tags_y = pad(test_tags_y, MAX_LENGTH)
+validation_tags_y = pad(validation_tags_y, MAX_LENGTH)
+
+model = get_model(word2index, tag2index)
+
+cat_train_tags_y = to_categorical(train_tags_y, len(tag2index))
+cat_test_tags_y = to_categorical(test_tags_y, len(tag2index))
+cat_validation_tags_y = to_categorical(validation_tags_y, len(tag2index))
+
+model.fit(train_sentences_X,
+          cat_train_tags_y,
+          batch_size=128,
+          epochs=40,
+          validation_data=(validation_sentences_X, cat_validation_tags_y))
+
+scores = model.evaluate(test_sentences_X, cat_test_tags_y)
 print(f"{model.metrics_names[1]}: {scores[1] * 100}")   # acc: 99.09751977804825
 
 model.save('model.h5')
-
-from keras.models import load_model
-model = load_model('model.h5')
