32c49f3fcaad70cb60bc480736e8671aabb0d67b
Arash Zandi <arash.zandi@gmail.com>
2020-06-21 12:03:13 +0200

    Remove unused utility methods

--- utils.py
+++ utils.py
@@ -1,8 +1,4 @@
 import numpy as np
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation
-from keras.optimizers import Adam
 
 def unzip(corpus):
     return split(tag_sentences(corpus))
@@ -54,52 +50,6 @@ def get_index2tag(tags):
         index2tag[i+1] = tag
     return index2tag
 
-def get_maps(sentences, tags):
-    words = get_unique_words(sentences)
-    tags = get_unique_tags(tags)
-    word2index = get_word2index(words)
-    tag2index = get_tag2index(tags)
-    index2tag = get_index2tag(tags)
-    return word2index, tag2index, index2tag
-
-def pad(data, length):
-    return pad_sequences(data, maxlen=length, padding='post')
-
-def get_x(sentences, word2index, maxlen):
-    x = []
-    for s in sentences:
-        s_int = []
-        for w in s:
-            try:
-                s_int.append(word2index[w.lower()])
-            except KeyError:
-                s_int.append(word2index['-OOV-'])
-    
-        x.append(s_int)
-    return pad_sequences(x, maxlen=maxlen, padding='post')
-
-def get_y(tags, tag2index, maxlen):
-    y = []
-    for s in tags:
-        y.append([tag2index[t] for t in s])
-    return pad_sequences(y, maxlen=maxlen, padding='post')
-
-def to_categorical(sequences, categories):
-    cat_sequences = []
-    for s in sequences:
-        cats = []
-        for item in s:
-            cats.append(np.zeros(categories))
-            cats[-1][item] = 1.0
-        cat_sequences.append(cats)
-    return np.array(cat_sequences)
-
-
-def get_xy(sentences, word2index, tags, tag2index, maxlen):
-    x = get_x(sentences, word2index, maxlen)
-    y = to_categorical(get_y(tags, tag2index, maxlen), len(tag2index))
-    return x, y
-
 def get_embedding_matrix(word2index):
     embeddings_index = {}
     with open('glove.6B.100d.txt', encoding="utf8") as glove_file:
@@ -114,22 +64,3 @@ def get_embedding_matrix(word2index):
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
     return embedding_matrix
-
-def get_model(word2index, tag2index, maxlen):
-    embedding_matrix = get_embedding_matrix(word2index)
-    model = Sequential()
-    model.add(InputLayer(input_shape=(maxlen, )))
-    model.add(Embedding(len(word2index) + 1,
-                        100,
-                        weights=[embedding_matrix],
-                        input_length=maxlen,
-                        trainable=True))
-    model.add(Bidirectional(LSTM(256, return_sequences=True)))
-    model.add(TimeDistributed(Dense(len(tag2index))))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                optimizer=Adam(learning_rate=0.001),
-                metrics=['accuracy'])
-    
-    model.summary()
-    return model
